faa85265de48215e0cb31953269b3f243aad99c5
Nic Durish <nicdurish@Lord-Macbook.local>
2019-05-07 16:22:25 -0400

    finally finished index

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,8 @@ $('.navTrigger').click(function () {
     $("#mainListDiv").fadeIn();
 
 });
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', '../assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
