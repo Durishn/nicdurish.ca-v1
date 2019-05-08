9a325c8c7516a3ab210d0c1cd09fb679b2001eab
Nic Durish <n.durish@gmail.com>
2019-05-08 08:50:08 -0400

    dropdown menus

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -16,15 +16,6 @@ $('.navTrigger').click(function () {
 
 });
 
-$(function() {
-  var $typed = $("#typed");
-  $typed.typed({
-    strings: [$typed.text()],
-    typeSpeed: 50,
-    startDelay: 1000
-  });
-});
-
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', '../assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
