2b9f38bb2582b08b7be0ec0c804232c082af6625
Nic Durish <nicdurish@Lord-Macbook.local>
2019-05-11 08:16:50 -0400

    some fixes, adding timeline, will fix later

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,6 @@ $(window).scroll(function() {
 });
 
 /*****************
-* Particle Animation
-*****************/
-particlesJS.load('particles-js', '../assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-
-
-/*****************
 * Mobile Dropdown Animation
 *****************/
 (function($) { 
@@ -34,6 +26,42 @@ particlesJS.load('particles-js', '../assets/particles.json', function() {
   });
 })(jQuery);
 
+
+
+/*****************
+* Timeline Animations
+*****************/
+$('.content').each( function(i){
+  
+  var bottom_of_object= $(this).offset().top + $(this).outerHeight();
+  var bottom_of_window = $(window).height();
+  
+  if( bottom_of_object > bottom_of_window){
+    $(this).addClass('hidden');
+  }
+});
+
+$(window).scroll( function(){
+    /* Check the location of each element hidden */
+    $('.hidden').each( function(i){
+      
+        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+      
+        /* If the object is completely visible in the window, fadeIn it */
+        if( bottom_of_window > bottom_of_object ){
+          $(this).animate({'opacity':'1'},700);
+        }
+    });
+});
+
+
+/*****************
+* Particle Animation
+*****************/
+particlesJS.load('particles-js', '../assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
 
 /*****************
 * Parallax Banner Animation
@@ -57,7 +85,5 @@ $(document).ready(function(){
     EasyPeasyParallax();
   });
 });
-
-
 
 
